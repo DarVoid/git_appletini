a46c5baabf66c4f65ad75f1dec54a1f876091abd
Jorge Duque <jorgemduque@hotmail.com>
2024-01-13 23:12:31 +0000

    why you so fyne

--- main.go
+++ main.go
@@ -9,34 +9,6 @@ import (
 	"fyne.io/fyne/v2/driver/desktop"
 )
 
-//go:embed assets/tray1.png
-var iconDefault []byte
-var resIconDefault = &fyne.StaticResource{
-	StaticName:    "tray1.png",
-	StaticContent: iconDefault,
-}
-
-//go:embed assets/tray2.png
-var iconReviewable []byte
-var resIconReviewable = &fyne.StaticResource{
-	StaticName:    "tray2.png",
-	StaticContent: iconReviewable,
-}
-
-//go:embed assets/tray3.png
-var iconMergeable []byte
-var resIconMergeable = &fyne.StaticResource{
-	StaticName:    "tray3.png",
-	StaticContent: iconMergeable,
-}
-
-//go:embed assets/tray4.png
-var iconBoth []byte
-var resIconBoth = &fyne.StaticResource{
-	StaticName:    "tray4.png",
-	StaticContent: iconBoth,
-}
-
 func main() {
 	a := app.New()
 
@@ -56,7 +28,7 @@ func main() {
 			}),
 			fyne.NewMenuItem("Show 2", func() {
 				fmt.Println("Clicked")
-				m.Items = m.Items[:1]
+				m.Items = m.Items[:1] // how to delete stuff
 				m.Refresh()
 			}),
 		}
